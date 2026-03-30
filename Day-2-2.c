72511706e435d22c716a165a3cfcdf177d0f40bf
Aiman Ashrafi <ashrafiaiman18@gmail.com>
2026-03-30 07:23:33 +0000

    day 3

--- Day-2-2.c
+++ Day-2-2.c
@@ -1,17 +1,29 @@
-int maxProfit(int* prices, int pricesSize) {
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int prices[n];
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &prices[i]);
+    }
+
     int min_price = prices[0];
     int max_profit = 0;
 
-    for (int i = 1; i < pricesSize; i++) {
-        if (prices[i] < min_price) {
+    for(int i = 1; i < n; i++) {
+        if(prices[i] < min_price) {
             min_price = prices[i];
         } else {
             int profit = prices[i] - min_price;
-            if (profit > max_profit) {
+            if(profit > max_profit) {
                 max_profit = profit;
             }
         }
     }
 
-    return max_profit;
+    printf("%d", max_profit);
+
+    return 0;
 }
---
+++ Day-3-2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);   // n is the expected size (including missing)
+
+    int arr[n-1];
+
+    for(int i = 0; i < n-1; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int total_sum = n * (n + 1) / 2;
+    int arr_sum = 0;
+
+    for(int i = 0; i < n-1; i++) {
+        arr_sum += arr[i];
+    }
+
+    int missing = total_sum - arr_sum;
+
+    printf("%d", missing);
+
+    return 0;
+}
